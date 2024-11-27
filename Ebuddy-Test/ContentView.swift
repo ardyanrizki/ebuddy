bc4fb0825b4c59ba968bae9ba602242204fc100b
Ardyan <ardyanrizki@icloud.com>
2024-11-27 18:31:54 +0700

    Initial Commit

---
+++ Ebuddy-Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ebuddy-Test
+//
+//  Created by Muhammad Rizki Ardyan on 27/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ebuddy-Test/Ebuddy_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ebuddy_TestApp.swift
+//  Ebuddy-Test
+//
+//  Created by Muhammad Rizki Ardyan on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Ebuddy_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
